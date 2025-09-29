936b0941916a5859c24898807f61b926e9c37140
Eliott Frohly <frohlyeliott@gmail.Com>
2025-09-29 16:15:28 +0200

    feat(main): added url parsing to main func

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from utils.data_loader import DataLoader
+from utils.url_parser import URLParser
 
 def main():
     dataset_id = 967
     data_loader = DataLoader(dataset_id)
     features, targets, metadata, variables = data_loader.load_data()
-    
+
+    url = "http://examplephishingsite.com"
+    parsed_url = URLParser.parse(url)
+
+    print("Parsed URL Domain Parts:", parsed_url)
     print("Features:", features)
     print("Targets:", targets)
     print("Metadata:", metadata)
     print("Variables:", variables)
 
-main()
+if __name__ == "__main__":
+    main()
